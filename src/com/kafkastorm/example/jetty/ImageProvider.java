a936aea7d7f7e35e55719477f878d46d654659e9
anton.l <antlitovka@gmail.com>
2015-12-21 00:53:57 +0200

    web page refresh

--- src/com/kafkastorm/example/jetty/ImageProvider.java
+++ src/com/kafkastorm/example/jetty/ImageProvider.java
@@ -1,14 +1,21 @@
 package com.kafkastorm.example.jetty;
 
 import com.kafkastorm.example.subscriber.ImageGenerator;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.handler.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.util.resource.Resource;
 
 /**
  * Created by anton.l on 12/20/2015.
@@ -20,30 +27,24 @@ public class ImageProvider extends AbstractHandler {
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
-                       HttpServletResponse response)
-            throws IOException, ServletException
-    {
-
-
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-
-
-        byte[] imageBytes = ImageGenerator.getWordsAndWriteToFile();
-        response.setHeader("Content-Type", "image/jpg");// or png or gif, etc
-        response.setHeader("Content-Length", "" + imageBytes.length);
-        response.getOutputStream().write(imageBytes);
-//        response.setContentType("text/html;charset=utf-8");
-//        response.setStatus(HttpServletResponse.SC_OK);
-//        baseRequest.setHandled(true);
-//        response.getWriter().println("<h1>Hello World " + i + "</h1>");
+                       HttpServletResponse response){
+        ImageGenerator.getWordsAndWriteToFile();
     }
 
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        server.setHandler(new ImageProvider());
 
+
+        Server server = new Server(8080);
+
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+
+        resource_handler.setResourceBase("src\\main\\webapp\\");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { new ImageProvider(), resource_handler});
+        server.setHandler(handlers);
         server.start();
         server.join();
     }
